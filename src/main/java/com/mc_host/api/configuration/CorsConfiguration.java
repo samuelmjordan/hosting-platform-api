1081abcd3874ac8e0515ef1626e94f34fa24aff9
Samuel Jordan <87397295+jordan12sam@users.noreply.github.com>
2025-02-07 22:48:30 +0000

    cors configuration

---
+++ src/main/java/com/mc_host/api/configuration/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.mc_host.api.configuration;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.Setter;
+
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfiguration implements WebMvcConfigurer {
+    List<String>  allowedOrigins;
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins(allowedOrigins.toArray(new String[0]))
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
+    }
+}
