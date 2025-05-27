b97059d25f2a6a486ef4510e966d8c4edf61efb7
Samuel Jordan <87397295+jordan12sam@users.noreply.github.com>
2025-05-27 11:13:30 +0100

    fix pterodactyl node create

--- src/main/java/com/mc_host/api/service/resources/v2/service/steps/PterodactylNodeStep.java
+++ src/main/java/com/mc_host/api/service/resources/v2/service/steps/PterodactylNodeStep.java
@@ -1,16 +1,27 @@
 package com.mc_host.api.service.resources.v2.service.steps;
 
+import com.mc_host.api.model.node.DnsARecord;
+import com.mc_host.api.model.node.PterodactylNode;
+import com.mc_host.api.repository.NodeRepository;
 import com.mc_host.api.repository.ServerExecutionContextRepository;
+import com.mc_host.api.service.resources.PterodactylService;
 import com.mc_host.api.service.resources.v2.context.Context;
 import com.mc_host.api.service.resources.v2.context.StepTransition;
 import com.mc_host.api.service.resources.v2.context.StepType;
 
 public class PterodactylNodeStep extends AbstractStep {
 
+    private final NodeRepository nodeRepository;
+    private final PterodactylService pterodactylService;
+
     protected PterodactylNodeStep(
-        ServerExecutionContextRepository contextRepository
+        ServerExecutionContextRepository contextRepository,
+        NodeRepository nodeRepository,
+        PterodactylService pterodactylService
     ) {
         super(contextRepository);
+        this.nodeRepository = nodeRepository;
+        this.pterodactylService = pterodactylService;
     }
 
     @Override
@@ -20,6 +31,11 @@ public class PterodactylNodeStep extends AbstractStep {
 
     @Override
     public StepTransition create(Context context) {
+        DnsARecord dnsARecord = nodeRepository.selectDnsARecord(context.getSubscriptionId())
+            .orElseThrow(() -> new IllegalStateException("DNS A Record not found for subscription: " + context.getSubscriptionId()));
+        PterodactylNode pterodactylNode = pterodactylService.createNode(dnsARecord);
+        nodeRepository.insertPterodactylNode(pterodactylNode);
+
         return inProgress(context, StepType.CONFIGURE_NODE);
     }
 
