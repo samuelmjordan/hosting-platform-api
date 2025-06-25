baf63ccde59995095815784f227c23183db7d118
jordan12sam <jordan12sam@gmail.com>
2025-06-25 13:39:17 +0100

    sftp auth

--- src/main/java/com/mc_host/api/controller/api/subscriptions/panel/SftpController.java
+++ src/main/java/com/mc_host/api/controller/api/subscriptions/panel/SftpController.java
@@ -1,19 +1,20 @@
 package com.mc_host.api.controller.api.subscriptions.panel;
 
+import com.mc_host.api.auth.CurrentUser;
+import com.mc_host.api.auth.ValidatedSubscription;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/panel/user/{userId}/subscription/{subscriptionId}/sftp")
+@RequestMapping("api/panel/user/subscription/{subscriptionId}/sftp")
 public interface SftpController {
 
-	@GetMapping("/credentials")
+	@GetMapping("credentials")
 	ResponseEntity<CredentialsResponse> getCredentials(
-		@PathVariable String userId,
-		@PathVariable String subscriptionId
+		@CurrentUser String userId,
+		@ValidatedSubscription String subscriptionId
 	);
 
 	record CredentialsResponse(
